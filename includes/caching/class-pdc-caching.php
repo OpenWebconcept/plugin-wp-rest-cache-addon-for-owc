1a413e912aa5c976a4275226ff84bbca30ec1565
Eyal Beker <eyal@acato.nl>
2025-11-20 14:00:56 +0100

    HLT-147 Delete cache relations on post save of specific post types.

--- includes/caching/class-pdc-caching.php
+++ includes/caching/class-pdc-caching.php
@@ -94,6 +94,9 @@ class Pdc_Caching extends Owc_Caching {
 
 			add_action( 'update_option__owc_pdc_base_settings', [ $this, 'clear_settings_cache' ] );
 		}
+
+		// Delete cache relations on post save of specific post types.
+		add_action( 'save_post', [ $this, 'delete_cache_relations_on_post_save' ], 10, 3 );
 	}
 
 	/**
@@ -174,5 +177,107 @@ class Pdc_Caching extends Owc_Caching {
 	 */
 	public function clear_settings_cache() {
 		\WP_Rest_Cache_Plugin\Includes\Caching\Caching::get_instance()->delete_object_type_caches( 'pdc-settings' );
+	}
+
+	/**
+	 * Delete cache relations when a post is saved.
+	 *
+	 * @param int      $post_id Post ID.
+	 * @param \WP_Post $post Post object.
+	 * @param bool     $update Whether this is an existing post being updated or not.
+	 *
+	 * @return void
+	 */
+	public function delete_cache_relations_on_post_save( $post_id, $post, $update ) {
+		if ( 'publish' !== $post->post_status ) {
+			return;
+		}
+
+		// Check if the saved post is a new post or an update. The $update parameter isn't doing what it's supposed to be doing,
+		// because of the auto-drafts that WordPress is creating.
+		// Therefore, $update also returns true on new posts, therefore we check the post date.
+		$post_date    = strtotime( $post->post_date );
+		$current_time = time();
+		$new_post     = false;
+		if ( ( $current_time - $post_date ) < 10 ) {
+			// New post, because the post date is within the last 10 seconds.
+			$new_post = true;
+		}
+
+		// Only proceed if it's a new post.
+		if ( ! $new_post ) {
+			return;
+		}
+
+		$caching           = Caching::get_instance();
+		$pdc_subcategories = [];
+
+		$post_type = get_post_type( $post_id );
+		switch ( $post_type ) {
+			case 'pdc-item':
+				// Check the related pdc-categories and pdc-subcategories.
+				$pdc_categories    = $this->get_related_posts( $post_id, 'pdc-item_to_pdc-category' );
+				$pdc_subcategories = $this->get_related_posts( $post_id, 'pdc-item_to_pdc-subcategory' );
+				break;
+			case 'pdc-subcategory':
+				// Check the related pdc-categories.
+				$pdc_categories = $this->get_related_posts( $post_id, 'pdc-category_to_pdc-subcategory' );
+				break;
+			default:
+				// do nothing.
+				return;
+		}
+
+		// Delete caches for related categories and subcategories.
+		if ( ! empty( $pdc_categories ) && is_array( $pdc_categories ) ) {
+			foreach ( $pdc_categories as $category ) {
+				$caching->delete_related_caches( $category->post_id, 'pdc-category' );
+			}
+		}
+
+		if ( ! empty( $pdc_subcategories ) && is_array( $pdc_subcategories ) ) {
+			foreach ( $pdc_subcategories as $subcategory ) {
+				$caching->delete_related_caches( $subcategory->post_id, 'pdc-subcategory' );
+			}
+		}
+	}
+
+	/**
+	 * Get related posts via p2p connections.
+	 *
+	 * @param int    $post_id Post ID.
+	 * @param string $relation_type Relation type.
+	 *
+	 * @return array An array of related post IDs.
+	 */
+	private function get_related_posts( $post_id, $relation_type ) {
+		switch ( $relation_type ) {
+			case 'pdc-category_to_pdc-subcategory':
+				// We need to get the pdc-categories that are connected to this subcategory, therefore we select p2p_from where p2p_to = $post_id.
+				$select_column = 'p2p_from';
+				$where_column  = 'p2p_to';
+				break;
+			case 'pdc-item_to_pdc-subcategory':
+			case 'pdc-item_to_pdc-category':
+				// We need to get the pdc-subcategories or pdc-categories that are connected to this pdc item, therefore we select p2p_to where p2p_from = $post_id.
+				$select_column = 'p2p_to';
+				$where_column  = 'p2p_from';
+				break;
+			default:
+				return [];
+		}
+
+		global $wpdb;
+
+		$related_post_ids = $wpdb->get_results(
+			$wpdb->prepare(
+				// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared -- $select_column and $where_column are controlled.
+				"SELECT {$select_column} AS `post_id` FROM {$wpdb->prefix}p2p WHERE {$where_column} = %d AND p2p_type = %s",
+				$post_id,
+				$relation_type
+			)
+		);
+
+		return $related_post_ids;
 	}
 }
